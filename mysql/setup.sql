beb9967f6f6fb5f9216651db82275054eb912760
Rugvedpednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2024-12-17 04:54:00 -0500

    Update setup.sql

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,1 +1,6 @@
-CREATE
+CREATE DATABASE IF NOT EXISTS passwords;
+CREATE USER 'passwords_user'@'localhost' IDENTIFIED BY 'k(D2Whiue9d8yD';
+GRANT ALL PRIVILEGES ON passwords.* TO 'passwords_user'@'localhost';
+USE passwords;
+
+SOURCE create_users_table.sql
