bc5a97100c13114b29f6e8c5da18db927fed4454
Rugvedpednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2024-12-17 15:05:32 -0500

    Update populate_accounts_table.sql

--- mysql/populate_accounts_table.sql
+++ mysql/populate_accounts_table.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO accounts (user_id, website_name, url, comment) VALUES
+(1, 'Google', 'https://www.google.com', 'Primary search engine'),
+(2, 'Facebook', 'https://www.facebook.com', 'Social media platform');
