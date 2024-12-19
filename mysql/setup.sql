7377365d8b5d39f487a1020399cc722c55d5cd69
Rugvedpednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2024-12-19 02:12:51 -0500

    Update setup.sql

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE IF NOT EXISTS passwords;
-CREATE USER 'passwords_user'@'localhost' IDENTIFIED BY 'k(D2Whiue9d8yD';
+\W -- Enable all warnings
+DROP DATABASE IF EXISTS `passwords`;
+CREATE DATABASE IF NOT EXISTS `passwords` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+CREATE USER IF NOT EXISTS 'passwords_user'@'localhost' IDENTIFIED BY 'k(D2Whiue9d8yD';
 GRANT ALL PRIVILEGES ON passwords.* TO 'passwords_user'@'localhost';
 USE passwords;
 
-SOURCE create_users_table.sql
-SOURCE create_accounts_table.sql
-SOURCE create_passwords_table.sql
-SOURCE populate_users_table.sql
-
+-- Source external SQL files (ensure correct order)
+SOURCE create-users-table.sql;       -- Create Users Table
+SOURCE create-accounts-table.sql;    -- Create Accounts Table
+SOURCE populate-users-table.sql;     -- Insert data into Users Table
+SOURCE populate-accounts-table.sql;  -- Insert data into Accounts Table
