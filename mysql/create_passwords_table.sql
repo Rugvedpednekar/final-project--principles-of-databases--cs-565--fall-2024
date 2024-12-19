056cdde0040e87dcc2684812bb7411872f081a27
Rugvedpednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2024-12-19 02:07:52 -0500

    Remove create_passwords_table.sql

--- mysql/create_passwords_table.sql
+++ mysql/create_passwords_table.sql
@@ -1,7 +1,1 @@
-CREATE TABLE passwords (
-    password_id INT AUTO_INCREMENT PRIMARY KEY,
-    account_id INT NOT NULL,
-    encrypted_password VARBINARY(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
-);
+
