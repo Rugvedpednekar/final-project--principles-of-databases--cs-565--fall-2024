b9ff558876f0ea828cd3a5bc260ab1e1755a4380
Rugvedpednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2024-12-19 02:12:02 -0500

    Update populate_users_table.sql

--- mysql/populate_users_table.sql
+++ mysql/populate_users_table.sql
@@ -1,3 +1,11 @@
-INSERT INTO users (first_name, last_name, username, email) VALUES
-('John', 'Doe', 'johndoe', 'john@example.com'),
-('Jane', 'Smith', 'janesmith', 'jane@example.com');
+INSERT INTO users (first_name, last_name, username, email) VALUES 
+('Salman', 'Khan', 'salman.khan', 'salman.khan@email.com'),
+('ShahRukh', 'Khan', 'shahrukh.khan', 'shahrukh.khan@email.com'),
+('Aamir', 'Khan', 'aamir.khan', 'aamir.khan@email.com'),
+('Ranbir', 'Kapoor', 'ranbir.kapoor', 'ranbir.kapoor@email.com'),
+('Hrithik', 'Roshan', 'hrithik.roshan', 'hrithik.roshan@email.com'),
+('Deepika', 'Padukone', 'deepika.padukone', 'deepika.padukone@email.com'),
+('Priyanka', 'Chopra', 'priyanka.chopra', 'priyanka.chopra@email.com'),
+('Katrina', 'Kaif', 'katrina.kaif', 'katrina.kaif@email.com'),
+('Alia', 'Bhatt', 'alia.bhatt', 'alia.bhatt@email.com'),
+('Kareena', 'Kapoor', 'kareena.kapoor', 'kareena.kapoor@email.com');
